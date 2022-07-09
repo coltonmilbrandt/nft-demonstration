632b3fbdc2578754009e12083905b19e87af0e94
coltonmilbrandt <coltonmilbrandt@gmail.com>
2022-07-09 16:54:17 -0600

    simple collectible functionality

---
+++ README.md
@@ -0,0 +1,3 @@
+1. Upload image to IPFS
+2. Make it decentralized
+3. Let's make it verifiably scarce and random
---
+++ scripts/helpful_scripts.py
@@ -0,0 +1,13 @@
+from brownie import accounts, network, config
+
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local", "local-ganache"]
+
+def get_account(index=None, id=None):
+    if index:
+        return accounts[index]
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS or network.show_active() in FORKED_LOCAL_ENVIRONMENTS:
+        return accounts[0]
+    if id:
+        return accounts.load(id)
+    return accounts.add(config["wallets"]["from_key"])

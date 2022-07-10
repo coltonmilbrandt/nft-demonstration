719f8065eea30e64940ce3ff3b4931c6808f0692
coltonmilbrandt <coltonmilbrandt@gmail.com>
2022-07-10 11:33:58 -0600

    adjustments for development mocks

--- scripts/advanced_collectible/deploy_and_create.py
+++ scripts/advanced_collectible/deploy_and_create.py
@@ -1,9 +1,15 @@
-from brownie import accounts, AdvancedCollectible
-from scripts.helpful_scripts import get_account, OPENSEA_URL
+from brownie import accounts, AdvancedCollectible, network, config
+from scripts.helpful_scripts import get_account, OPENSEA_URL, get_contract
 
 def main():
     deploy_and_create()
 
 def deploy_and_create():
     account = get_account()
-    advanced_collectible = AdvancedCollectible.deploy({"from": account})
+    advanced_collectible = AdvancedCollectible.deploy(
+        get_contract("vrf_coordinator"),
+        get_contract("link_token"),
+        config["networks"][network.show_active()]["keyhash"],
+        config["networks"][network.show_active()]["fee"],
+        {"from": account}
+    )
--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,4 +1,4 @@
-from brownie import accounts, network, config
+from brownie import accounts, network, config, Contract, VRFCoordinatorMock, LinkToken
 
 FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local", "local-ganache"]
@@ -12,3 +12,52 @@ def get_account(index=None, id=None):
     if id:
         return accounts.load(id)
     return accounts.add(config["wallets"]["from_key"])
+
+contract_to_mock = {
+    "vrf_coordinator": VRFCoordinatorMock,
+    "link_token": LinkToken
+}
+
+def get_contract(contract_name):
+    """ This function will grab the contract addresses from the brownie config programatically
+
+        Args: 
+            contract_name (string)
+
+        Returns:
+            brownie.network.contract.ProjectContract: The most recently deployed version of this contract
+    
+    """
+
+    contract_type = contract_to_mock[contract_name]
+
+    # We need to create a mock price feed if in a local environment
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        if len(contract_type) <= 0:
+            # MockV3Aggregator.length
+            deploy_mocks()
+        contract = contract_type[-1]
+        # MockV3Aggregator[-1]
+    else:
+        contract_address = config["networks"][network.show_active()][contract_name]
+        # We will need, as always, to deploy a contract
+        # Address
+        # ABI
+        contract = Contract.from_abi(contract_type._name, contract_address, contract_type.abi)
+        # MockV3Aggregator.abi
+    return contract
+
+def deploy_mocks():
+    """
+    Use this script if you want to deploy mocks to a testnet
+    """
+    print(f"The active network in {network.show_active()}")
+    print("Deploying mocks...")
+    account = get_account()
+    print("Deploying Mock Link Token...")
+    link_token = LinkToken.deploy({"from": account})
+    print(f"Link Token deployed to {link_token.address}")
+    print("Deploying Mock VRFCoordinator...")
+    vrf_coordinator = VRFCoordinatorMock.deploy(link_token.address, {"from": account})
+    print(f"VRFCoordinator deployed to {vrf_coordinator.address}")
+    print("Deployed")
